f11a521ec3870d48befeaca6f6630e2256bc46ba
Kevin Dalton <kmdalton@fas.harvard.edu>
2018-01-10 07:41:02 -0800

    timetool,outdir options added to experiment_params.py

--- experiment_params.py
+++ experiment_params.py
@@ -1,2 +1,12 @@
-#Right now this is just needed to sync the data directories between scripts
-experiment_name = "xppls5316"
+#For LS53
+#experiment_name = "xppls5316"
+#timetool = False
+#outdir = '/reg/d/psdm/xpp/{:s}/scratch/hdf5_compressed'.format(experiment_name)
+#phi_name = 'hekstra_phi'
+
+
+#For LQ92
+experiment_name = "xpplq9215"
+timetool = True
+outdir = '/reg/d/psdm/xpp/{:s}/scratch/hdf5_compressed'.format(experiment_name)
+phi_name = 'sam_rot'
